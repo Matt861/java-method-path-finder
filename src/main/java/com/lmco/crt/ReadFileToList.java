47ffa0596d61ee9f7fcb6cc5d60c019d1c304d6a
Matt861 <Matt861@mattwindham21@gmail.com>
2024-07-22 13:55:16 -0500

    initial commit

---
+++ src/main/java/com/lmco/crt/ReadFileToList.java
@@ -0,0 +1,40 @@
+package com.lmco.crt;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFileToList {
+
+    public static List<String> readFileToList(String filePath) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static List<String> readFileFromResources(String fileName) {
+        List<String> lines = new ArrayList<>();
+        ClassLoader classLoader = ReadFileToList.class.getClassLoader();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+            assert inputStream != null;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    lines.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
